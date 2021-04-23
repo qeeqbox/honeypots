586634f90ac51d05917ab6395a2c774155d782c7
G <52905881+giga-a@users.noreply.github.com>
2021-04-23 11:02:00 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='gigaqeeq@gmail.com',
     description="Easy to setup honeypots!",
     long_description=long_description,
-    version='0.8',
+    version='0.11',
     license="AGPL-3.0",
     url="https://github.com/qeeqbox/honeypots",
     packages=['honeypots'],
