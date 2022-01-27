bc870eebb6fac3e8336148a9410765b038c6983d
Giga <52905881+giga-a@users.noreply.github.com>
2022-01-26 16:12:59 -0800

    Add files via upload

--- honeypots/testing.py
+++ honeypots/testing.py
@@ -1,12 +1,14 @@
 import honeypots
 from time import sleep
+from pkg_resources import get_distribution
+print("Version: ",get_distribution('honeypots').version)
 for server, cls in honeypots.__dict__.items():
     if server.endswith('Server'):
         print("Start testing {}".format(server))
         temp_server = cls()
-        #temp_server.run_server(process=True, auto=True)
+        temp_server.run_server(process=True, auto=True)
         sleep(2)
-        temp_server.test_server(port=21)
+        temp_server.test_server()
         temp_server.kill_server()
         print("Done testing {}".format(server))
 honeypots.clean_all()
