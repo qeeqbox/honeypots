76f0934af5da45b63faa7bf9ca0100c23afb6eec
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-02-14 17:59:00 +0100

    oracle server: ruff fixes

--- honeypots/oracle_server.py
+++ honeypots/oracle_server.py
@@ -10,9 +10,10 @@
 //  -------------------------------------------------------------
 """
 
+from __future__ import annotations
+
 from contextlib import suppress
 from re import findall
-from struct import unpack
 
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
@@ -27,49 +28,56 @@ class QOracleServer(BaseServer):
     NAME = "oracle_server"
     DEFAULT_PORT = 1521
 
-    def server_main(self):
+    def server_main(self):  # noqa: C901
         _q_s = self
 
         class CustomRedisProtocol(Protocol):
             _state = None
 
-            def wrong_password(self):
-                payload = b"\x02B\xc5\xbb\xe7\x7f\x02B\xac\x11\x00\x02\x08\x00E\x00\x01\x02Md@\x00@\x06\x94l\xac\x11\x00\x02\xac\x11\x00\x01\x05\xf1\xa5\xa8\xab\xf5\xff\x94\x98\xdf\xd5\xa1\x80\x18\x01\xf5Y\x1a\x00\x00\x01\x01\x08\nJ\xe7\xf0,\xb2,\xfe\x08\x00\x00\x00\xce\x06\x00\x00\x00\x00\x00\x04\x01\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\xf9\x03\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x006\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x82\x1c\x86u\x7f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf9\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x003ORA-01017: invalid username/password; logon denied\n"
-                return payload
+            @staticmethod
+            def wrong_password() -> bytes:
+                return (
+                    b"\x02B\xc5\xbb\xe7\x7f\x02B\xac\x11\x00\x02\x08\x00E\x00\x01\x02Md@\x00@\x06"
+                    b"\x94l\xac\x11\x00\x02\xac\x11\x00\x01\x05\xf1\xa5\xa8\xab\xf5\xff\x94\x98"
+                    b"\xdf\xd5\xa1\x80\x18\x01\xf5Y\x1a\x00\x00\x01\x01\x08\nJ\xe7\xf0,\xb2,\xfe"
+                    b"\x08\x00\x00\x00\xce\x06\x00\x00\x00\x00\x00\x04\x01\x00\x00\x00\x00\x00"
+                    b"\x01\x00\x00\x00\x00\xf9\x03\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x006\x01\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x82\x1c\x86u\x7f\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf9\x03\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x00\x003ORA-01017: "
+                    b"invalid username/password; logon denied\n"
+                )
 
-            def refuse_payload(self):
-                payload = b"\x00\x08\x00\x00\x04\x00\x00\x00"
-                return payload
+            @staticmethod
+            def refuse_payload() -> bytes:
+                return b"\x00\x08\x00\x00\x04\x00\x00\x00"
 
-            def parse_payload(self, data):
+            @staticmethod
+            def parse_payload(data: bytes) -> tuple[str | None, str | None, str | None]:
                 service_name = None
                 program = None
                 local_user = None
                 with suppress(Exception):
-                    (
-                        packet_len,
-                        packet_checksum,
-                        packet_type,
-                        packet_reserved_bytes,
-                        packet_header_checksum,
-                    ) = unpack(">hhbbh", data[0:8])
-                    if b"(DESCRIPTION=" in data:
-                        connect = data[data.index(b"(DESCRIPTION=") :].split(b"\0")[0]
-                        found_temp = findall(rb"[^\(\)]+", connect)
-                        if len(found_temp) > 0:
-                            found_fixed = [item for item in found_temp if not item.endswith(b"=")]
-                            if len(found_fixed) > 0:
-                                for item in found_fixed:
-                                    name, value = item.split(b"=")
-                                    if name.startswith(b"SERVICE_NAME"):
-                                        service_name = value.decode()
-                                    elif name.startswith(b"PROGRAM"):
-                                        program = value.decode()
-                                    elif name.startswith(b"USER"):
-                                        local_user = value.decode()
+                    offset = data.index(b"(DESCRIPTION=")
+                    connect = data[offset:].split(b"\0")[0]
+                    attributes = [
+                        item for item in findall(rb"[^\(\)]+", connect) if not item.endswith(b"=")
+                    ]
+                    for item in attributes:
+                        name, value = item.split(b"=", maxsplit=1)
+                        if name.startswith(b"SERVICE_NAME"):
+                            service_name = value.decode()
+                        elif name.startswith(b"PROGRAM"):
+                            program = value.decode()
+                        elif name.startswith(b"USER"):
+                            local_user = value.decode()
                 return service_name, program, local_user
 
-            def connectionMade(self):
+            def connectionMade(self):  # noqa: N802
                 _q_s.log(
                     {
                         "action": "connection",
@@ -78,7 +86,7 @@ class QOracleServer(BaseServer):
                     }
                 )
 
-            def dataReceived(self, data):
+            def dataReceived(self, data: bytes):  # noqa: N802
                 service_name, program, local_user = self.parse_payload(data)
                 if service_name or program or local_user:
                     _q_s.log(
@@ -101,14 +109,25 @@ class QOracleServer(BaseServer):
         reactor.listenTCP(port=self.port, factory=factory, interface=self.ip)
         reactor.run()
 
-    def test_server(self, ip=None, port=None, username=None, password=None):
+    def test_server(self, ip=None, port=None, username=None, password=None):  # noqa: ARG002
         with suppress(Exception):
             from warnings import filterwarnings
 
             filterwarnings(action="ignore", module=".*socket.*")
             from socket import socket, AF_INET, SOCK_STREAM
 
-            payload = b"\x00\x00\x03\x04\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00E\x00\x01F\xb9\xd9@\x00@\x06\x81\xd6\x7f\x00\x00\x01\x7f\x00\x00\x01\xbf\xce\x06\x13\xacW\xde\xc0Z\xb5\x0cI\x80\x18\x02\x00\xff:\x00\x00\x01\x01\x08\n\x1bdZ^\x1bdZ^\x01\x12\x00\x00\x01\x00\x00\x00\x01>\x01,\x0cA \x00\xff\xff\x7f\x08\x00\x00\x01\x00\x00\xc8\x00J\x00\x00\x14\x00AA\xa7C\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x01(DESCRIPTION=(CONNECT_DATA=(SERVICE_NAME=xe)(CID=(PROGRAM=linux_1)(HOST=xxxxxxxxxxxxxx)(USER=xxxxxxxxxxxxxx))(CONNECTION_ID=xxxxxxxxxxxxxxxxxxxxxxxx))(ADDRESS=(PROTOCOL=tcp)(HOST=xxxxxxx)(PORT=xxxx)))"
+            payload = (
+                b"\x00\x00\x03\x04\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00E\x00\x01F\xb9"
+                b"\xd9@\x00@\x06\x81\xd6\x7f\x00\x00\x01\x7f\x00\x00\x01\xbf\xce\x06\x13\xacW\xde"
+                b"\xc0Z\xb5\x0cI\x80\x18\x02\x00\xff:\x00\x00\x01\x01\x08\n\x1bdZ^\x1bdZ^\x01\x12"
+                b"\x00\x00\x01\x00\x00\x00\x01>\x01,\x0cA \x00\xff\xff\x7f\x08\x00\x00\x01\x00"
+                b"\x00\xc8\x00J\x00\x00\x14\x00AA\xa7C\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x00\x00 \x00\x00\x00"
+                b"\x00\x00\x00\x00\x00\x00\x01(DESCRIPTION=(CONNECT_DATA=(SERVICE_NAME=xe)"
+                b"(CID=(PROGRAM=linux_1)(HOST=xxxxxxxxxxxxxx)(USER=xxxxxxxxxxxxxx))"
+                b"(CONNECTION_ID=xxxxxxxxxxxxxxxxxxxxxxxx))(ADDRESS=(PROTOCOL=tcp)"
+                b"(HOST=xxxxxxx)(PORT=xxxx)))"
+            )
             _ip = ip or self.ip
             _port = port or self.port
             c = socket(AF_INET, SOCK_STREAM)
