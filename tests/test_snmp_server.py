23539388a7ae1ee9c41b8bf02b2ec7c2d614b00d
Giga <52905881+giga-a@users.noreply.github.com>
2024-09-24 11:27:08 -0700

    Update test_snmp_server.py

--- tests/test_snmp_server.py
+++ tests/test_snmp_server.py
@@ -15,28 +15,21 @@ from honeypots import QSNMPServer
 from .utils import (
     assert_connect_is_logged,
     IP,
+    connect_to,
     load_logs_from_file,
     wait_for_server,
 )
 
 PORT = "50161"
 
-
 @pytest.mark.parametrize(
     "server_logs",
     [{"server": QSNMPServer, "port": PORT}],
     indirect=True,
 )
-def test_snmp_server(server_logs):
-    with wait_for_server(PORT):
-        g = getCmd(
-            SnmpEngine(),
-            CommunityData("public"),
-            UdpTransportTarget((IP, int(PORT))),
-            ContextData(),
-            ObjectType(ObjectIdentity("1.3.6.1.4.1.9.9.618.1.4.1.0")),
-        )
-        next(g)
+def test_postgres_server(server_logs):
+    with wait_for_server(PORT), connect_to(IP, PORT, udp=True) as connection:
+        connection.send(b'\x30\x2e\x02\x01\x01\x04\x06\x70\x75\x62\x6c\x69\x63\xa0\x21\x02\x04\x00\xab\x61\x67\x02\x01\x00\x02\x01\x00\x30\x13\x30\x11\x06\x0d\x2b\x06\x01\x04\x01\x09\x09\x84\x6a\x01\x04\x01\x00\x05\x00')
 
     logs = load_logs_from_file(server_logs)
 
